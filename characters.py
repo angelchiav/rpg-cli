60e9b199622570cce4ff1b381f32d315e1f9763f
angelchiav <achiavicuna@gmail.com>
2025-09-12 20:28:59 -0300

    add classes

--- characters.py
+++ characters.py
@@ -1,13 +1,12 @@
-from enums import Classes
-
 class Player:
-    def __init__(self, health=100, money=0, strength=0, defense=0, mana=0, xp=0):
+    def __init__(self, health=100, money=0, strength=0, defense=0, mana=0, xp=0, level=1):
         self.__health = health
         self.__money = money
         self.__strength = strength
         self.__defense = defense
         self.__mana = mana
         self.__xp = xp
+        self.__level = level
         self.__inventory = []
 
     def get_money(self):
@@ -24,6 +23,9 @@ class Player:
     
     def get_inventory(self):
         return self.__inventory
+    
+    def get_xp(self):
+        return self.__xp
     
     def set_money(self, value):
         if value < 0:
@@ -45,6 +47,11 @@ class Player:
             raise Exception("Negative value is not allowed")
         self.__defense += value
     
+    def set_xp(self, value):
+        self.__xp = self.__xp - value
+        if self.__xp < 0:
+            raise Exception("You can't have negative experience")
+    
     def set_inventory(self, item):
         if item not in self.__inventory:
             raise Exception("You already have this item")
@@ -65,5 +72,34 @@ class Player:
             print(f"You were hit with {value} points of damage.  Your total health {self.__health}")
         else:
             self.kill()
-        
+
+    def level_up(self):
+        self.__level += 1
+    
+    def become_warrior(self):
+        self.__health = 100
+        self.__money = 0
+        self.__strength = 10
+        self.__defense = 8
+        self.__mana = 2
+        self.__xp = 0
+        self.__inventory = ['WOODEN SWORD']
+
+    def become_archer(self):
+        self.__health = 80
+        self.__money = 0
+        self.__strength = 8
+        self.__defense = 6
+        self.__mana = 7
+        self.__xp = 0
+        self.__inventory = ['WOODEN ARCH']
+    
+    def become_wizard(self):
+        self.__health = 100
+        self.__money = 0
+        self.__strength = 4
+        self.__defense = 8
+        self.__mana = 15
+        self.__xp = 0
+        self.__inventory = ['WOODEN WAND']
     
--- enums.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class Classes(Enum):
-    WARRIOR = "warrior"
-    ARCHER = "archer"
-    WIZARD = "wizard"
